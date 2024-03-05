0d369cda981110eaf862cdcf28ea483316d39de5
kamily <kamily7803@gmail.com>
2024-03-05 16:27:35 -0300

    feat: photo post

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,47 +1,53 @@
 import React from 'react';
 
 const types = {
-    email: {
-        regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        message: 'Preencha um email válido',
-    },
-    password: {
-        regex: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/,
-        message: 'A senha precisa ter no mínimo 8 caracteres, sendo pelo menos 1 maiúsculo, 1 número e 1 símbolo.'
-    }
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido',
+  },
+  password: {
+    regex: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/,
+    message:
+      'A senha precisa ter no mínimo 8 caracteres, sendo pelo menos 1 maiúsculo, 1 número e 1 símbolo.',
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas',
+  },
 };
 
 const useForm = (type) => {
-    const [value, setValue] = React.useState("");
-    const [error, setError] = React.useState(null);
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
 
-    function validate(value) {
-        if (type === false) return true;
-        if (value.length === 0) {
-            setError('Preencha um valor');
-            return false;
-        } else if (types[type] && !types[type].regex.test(value)) {
-            setError(types[type].message);
-            return false;
-        } else {
-            setError(null);
-            return true;
-        }
-    }    
-
-    function onChange({target}) {
-        if(error) validate(target.value);
-        setValue(target.value);
-    } 
-
-    return {
-        value,
-        setValue,
-        onChange,
-        error,
-        validate: () => validate(value),
-        onBlur: () => validate(value),
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
     }
-}
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
 
 export default useForm;

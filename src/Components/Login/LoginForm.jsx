296f71bba4e1b73d74126645db3f672f0ed9ca4b
kamily <kamily7803@gmail.com>
2024-03-07 14:01:34 -0300

    feat: add reactive head title

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -6,38 +6,44 @@ import useForm from '../../Hooks/useForm';
 import { UserContext } from '../../UserContext';
 import styles from './LoginForm.module.css';
 import stylesBtn from '../Forms/Button.module.css';
+import Head from '../Helper/Head';
 
 const LoginForm = () => {
   const userName = useForm();
   const password = useForm();
 
-  const {userLogin, error, loading} = React.useContext(UserContext);
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    if(userName.validate() && password.validate()) {
+    if (userName.validate() && password.validate()) {
       userLogin(userName.value, password.value);
     }
   }
 
   return (
-    <section className='animeLeft'>
-      <h1 className='title'>Login</h1>
+    <section className="animeLeft">
+      <Head title="Login" />
+      <h1 className="title">Login</h1>
       <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="userName" {...userName} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? <Button>Carregando...</Button> : <Button>Entrar</Button>}
         {error && <p>{error}</p>}
       </form>
-      <Link className={styles.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a Senha?
+      </Link>
       <div className={styles.cadastro}>
         <h2 className={styles.subtitle}>Cadastre-se</h2>
         <p>Ainda não possui conta? Cadastre-se no site.</p>
-        <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
